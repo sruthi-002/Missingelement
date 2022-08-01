29394b0510f8ce99411acaa6ce3d646c17afd90e
sruthi-002 <89520373+sruthi-002@users.noreply.github.com>
2022-08-01 11:49:21 +0530

    Add files via upload

---
+++ Missingelement.java
@@ -0,0 +1,33 @@
+﻿package com.company;
+import java.util.Scanner;
+class missing
+{
+    public void  missingelement()
+    {
+        int count=0;
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int i,j;
+        int []a = new int[100];
+        for(i=0;i<n;i++)
+            a[i]=in.nextInt();
+        for(i=1;i<=n;i++)
+        {
+            for(j=0;j<n;j++) {
+                if (a[j] == i)
+                    count++;
+                }
+            if (count == 0) {
+                System.out.println("The missing element is " + " " + i);
+                break;
+            }
+            count=0;
+        }
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        missing m =new missing();
+        m.missingelement();
+    }
+}
